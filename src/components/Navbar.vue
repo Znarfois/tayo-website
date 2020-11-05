2d9d6f3a20ef82c34fef7c99b4d3ecf9fbeb0075
Franz Taborlupa <franztaborlupa@gmail.com>
2020-11-06 04:35:33 +0800

    Start Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav>
+        <div class="nav-primary">
+            <img src="#" alt="TAYO logo">
+        </div>
+        <div class="nav-secondary">
+            <a href="#">Home</a>
+            <a href="#">Services</a>
+            <a href="#">Projects</a>
+            <a href="#">Who We Are</a>
+            <a href="#">Contact Us</a>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style scoped>
+    nav a {
+        text-decoration: none;
+        color: #000000;
+    }
+
+    nav {
+        display: flex;
+        padding: 48px 0;
+        justify-content: space-between;
+    }
+
+</style>
