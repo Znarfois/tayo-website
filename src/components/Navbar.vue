f11e552675cc3163124d8d44d64c5fdf452849cc
Franz Taborlupa <franztaborlupa@gmail.com>
2020-12-03 21:45:35 +0800

    Add black tayo logo + Hover states

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,16 @@
 <template>
     <nav id='nav'>
-        <div class="nav-primary">
-            <img src="../assets/img/tayo-logo.png" alt="TAYO logo">
-        </div>
-        <div class="nav-secondary">
-            <router-link to="/">Home</router-link>
-            <router-link to="/about">Who We Are</router-link>
-            <router-link to="/projects/kidsforkids">Projects</router-link>
-            <router-link to="/services">Services</router-link>
-            <router-link to="/contactus">Contact Us</router-link>
+        <div class='nav-content'>
+            <div class="nav-primary">
+                <router-link to="/"><img :src="require('../assets/img/' + brand + '')" id="tayo-logo" alt="TAYO logo"></router-link>
+            </div>
+            <div class="nav-secondary">
+                <router-link to="/">Home</router-link>
+                <router-link to="/about">Who We Are</router-link>
+                <router-link to="/projects/kidsforkids">Projects</router-link>
+                <router-link to="/services">Services</router-link>
+                <router-link to="/contactus">Contact Us</router-link>
+            </div>
         </div>
     </nav>
 </template>
@@ -16,7 +18,12 @@
 <script>
 
 export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    data() {
+        return {
+            brand: "tayo-logo.png",
+        }
+    }
 }
 // var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
 // if (scrollTop >= 40) {
@@ -28,53 +35,74 @@ export default {
 //     document.querySelector('nav').style.backgroundColor = 'none';
 //     document.querySelector('nav').style.position = 'static';
 // }
-var myNav = document.getElementById('nav');
+var myNav = document.getElementsByClassName('nav-content');
 window.onscroll = function () { 
     "use strict";
-    if (document.body.scrollTop >= 40 || document.documentElement.scrollTop >= 40 ) {
+    if (window.scrollY >= 40 ) {
         myNav.classList.add("nav-colored");
         myNav.classList.remove("nav-transparent");
     } 
-    else {
+    else if (window.scrollY < 40 ){
         myNav.classList.add("nav-transparent");
         myNav.classList.remove("nav-colored");
     }
-};
+
+    if(this.$route.path === "/") {
+        this.brand = "tayo-logo.png";
+    }
+
+    else if (this.$route.path !== "/") {
+        this.brand = "tayo-logo-black.png";
+        document.getElementsByTagName('a').style.color = '#000000';
+    }
+}
 </script>
 
 <style scoped>
     /* Import Fonts */
     @import url("https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap");
-    
-    nav a {
-        text-decoration: none;
-        color: #000000;
-        padding: 0 12px;
-    }
 
     nav {
         position: fixed;
+        display: block;
+        height: 72px;
+        width: 100%;
+        z-index: 4000;
+    }
+
+    nav a {
+        text-decoration: none;
+        color: #FFFFFF;
+        margin: 0 32px;
+        padding-bottom: 4px;
+    }
+
+    .nav-content {
         display: flex;
-        padding: 48px 24px;
         justify-content: space-between;
+        padding: 12px 52px;
+        align-items: center;
     }
 
     nav img {
         width: 48px;
     }
 
-    nav a:hover {
+    .nav-secondary a:hover {
         font-family: "Karla"; 
         /* font-weight: 700; */
-        text-decoration: underline;
+        border-bottom: 4px solid #EAA200;
     }
 
     .nav-transparent {
         background-color: transparent;
+        transition: background 0.25s ease-in-out 0s;
+        box-shadow: none;
     }
 
     .nav-colored {
         background-color: #FFFFFF;
-        z-index: 4000;
+        color: #000000;
+        box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 4px;
     }
 </style>
