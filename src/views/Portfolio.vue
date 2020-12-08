cee807c418fd170fefd51c975f84aa56ccb6b842
Franz Taborlupa <franztaborlupa@gmail.com>
2020-12-08 15:30:27 +0800

    Responsive

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -2,19 +2,36 @@
     <main> 
         <form>
             <h1>Request For Portfolio</h1>
+            <div class="input-info">
             <p>Full Name</p>
-            <input type="text">
-            <p>Email</p>
-            <input type="email">
-            <p>Phone</p>
-            <input type="text">
-            <p>Job/Position</p>
-            <input type="email">
-            <p>Company</p>
-            <input type="text">
+            <input type="text" class="full-width">
+            </div>
+            <div class="input-grid">
+                <div class="input-info">
+                    <p>Email</p>
+                    <input type="email">
+                </div>
+                <div class="input-info">
+                    <p>Phone</p>
+                    <input type="text">
+                </div>
+            </div>
+            <div class="input-grid">
+                <div class="input-info">
+                    <p>Job/Position</p>
+                    <input type="email">
+                </div>
+                <div class="input-info">
+                    <p>Company</p>
+                    <input type="text">
+                </div>
+            </div>
+            <div class="input-info">
             <p>Reason for Request</p>
-            <textarea></textarea>
+            <textarea class="full-width"></textarea>
+            </div>
             <button>Send Message</button>
+            <span>Expect a response within seven (7) working days.</span>
         </form>
     </main>
 </template>
@@ -26,7 +43,7 @@ export default {
 
 <style scoped>
     main {
-        padding: 80px 0 80px 10%;
+        padding: 80px 10% 80px 10%;
     }
 
     form {
@@ -50,32 +67,75 @@ export default {
         font-size: 14px;
         line-height: 1.2;
         padding: 10px;
-        width: 46%;
-        max-width: 1152px;
+        width: 100%;
         border: 2px solid #d2d2d2;
         border-radius: 5px;
         margin: 0 0 20px 0;
         opacity: 0.5;
     }   
 
-    form:nth-child(n + 2) > input {
-        width: 40%;
-        max-width: 560px;
+    .input-grid {
+        display: flex;
+        flex-direction: row;
     }
 
+    .input-grid .input-info {
+        width: 100%;
+    }
+
+    .input-grid .input-info:first-child{
+        padding: 0 32px 0 0;
+    }
+
+    /* p {
+        width: 40%;
+        display: inline-block;
+    } */
+
     textarea {
-        height: 14%;
-        margin-bottom: 20px
+        height: 168px;
+        margin-bottom: 72px;
+        width: 100%;
     }
 
     button {
+        display: block;
         width: 208px;
         height: 64px;
         background-color: #EAA200;
         color: #FFFFFF;
     }
 
+    form span {
+        display: block;
+        margin: 20px 0 0 0;
+        font-size: 16px;
+        line-height: 32px;
+        color: #333333;
+        opacity: 0.5;
+    }
+
+    .full-width {
+        width: 100%;
+    }
+
     /* textarea {
         rows: '4';
     } */
+
+    @media screen and (max-width: 768px) {
+        .input-grid {
+            flex-direction: column;
+        }
+
+        .input-grid .input-info:first-child{
+            padding: 0;
+        }
+
+        textarea {
+            height: 168px;
+            margin-bottom: 36px;
+            width: 100%;
+        }
+    }
 </style>
