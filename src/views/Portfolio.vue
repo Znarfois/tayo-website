159f8b405e7f1688f4f4232fdbb783176cd6c1da
Franz Taborlupa <franztaborlupa.tech@gmail.com>
2020-12-19 11:51:43 +0800

    Requests pushing to Firestore

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,43 +1,99 @@
 <template>
     <main> 
-        <form>
+        <form @submit.prevent="requestSend">
             <h1>Request For Portfolio</h1>
             <div class="input-info">
             <p>Full Name</p>
-            <input type="text" class="full-width">
+            <input type="text" class="full-width" v-model="name">
             </div>
             <div class="input-grid">
                 <div class="input-info">
                     <p>Email</p>
-                    <input type="email">
+                    <input type="email" v-model="email">
                 </div>
                 <div class="input-info">
                     <p>Phone</p>
-                    <input type="text">
+                    <input type="text" v-model="phone">
                 </div>
             </div>
             <div class="input-grid">
                 <div class="input-info">
                     <p>Job/Position</p>
-                    <input type="email">
+                    <input type="text" v-model="job">
                 </div>
                 <div class="input-info">
                     <p>Company</p>
-                    <input type="text">
+                    <input type="text" v-model="company">
                 </div>
             </div>
             <div class="input-info">
             <p>Reason for Request</p>
-            <textarea class="full-width"></textarea>
+            <textarea class="full-width" v-model="reason"></textarea>
             </div>
-            <button>Send Message</button>
+            <button type='submit'>Send Message</button>
             <span>Expect a response within seven (7) working days.</span>
         </form>
     </main>
 </template>
 
 <script>
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9Vsa6Md62rE_fK8cM5u5R0_L1ZQrn8iE",
+  authDomain: "tayo-c846e.firebaseapp.com",
+  databaseURL: "https://tayo-c846e.firebaseio.com",
+  projectId: "tayo-c846e",
+  storageBucket: "tayo-c846e.appspot.com",
+  messagingSenderId: "91886545753",
+  appId: "1:91886545753:web:9574d19c0fa60bbe80ba48",
+  measurementId: "G-6XESH3H38B"
+};
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+
 export default {
+    data: function () {
+        return {
+            contactForm: {
+                name: '',
+                email: '',
+                phone: '',
+                job: '',
+                company: '',
+                reason: '',
+            }
+        }
+    },
+    methods: {
+        
+        requestSend: function (evt) {
+            evt.preventDefault();
+
+            db.collection('Requests').add({
+            name: this.name,
+            email: this.email,
+            phone: this.phone,
+            job: this.job,
+            company: this.company,
+            reason: this.reason,
+            })
+            .then(function(){
+                console.log("Document successfully written!");
+            })
+            this.name = "";
+            this.email = "";
+            this.phone = "";
+            this.job = "";
+            this.company = "";
+            this.reason = "";
+
+            evt.target.reset();
+        }
+    }
 }
 </script>
 
